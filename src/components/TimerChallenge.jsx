f1f9dc6c51505c7da96611e6867150013e647b72
magan <maganpreetsr98@gmail.com>
2025-02-22 10:03:58 -0500

    first commit of project that uses refs

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,45 @@
+import { useRef, useState } from "react";
+import ResultModal from "./ResultModal";
+
+export default function TimerChallenge({ title, targetTime }) {
+
+  const timer = useRef();
+  const dialog = useRef();
+
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+      dialog.current.showModal();
+    }, targetTime * 1000);
+    setTimerStarted(true);
+  }
+
+  function handleStop() {
+    clearTimeout(timer.current);
+    setTimerStarted(false);
+  }
+
+  return (
+    <>
+      <ResultModal ref={dialog} result="lost" targetTime={targetTime}></ResultModal>
+      <section className="challenge">
+        <h2>{title}</h2>
+        {timerExpired && <p className="challenge-expired">You lost!</p>}
+        <p className="challenge-time">
+          {targetTime} second{targetTime > 1 ? "s" : ""}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {!timerStarted ? "Start" : "Stop"} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? "active" : ""}>
+          {timerStarted ? "Time is running..." : "Timer inactive"}
+        </p>
+      </section>
+    </>
+  );
+}
